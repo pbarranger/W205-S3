f90e99e3024f7e3070286b761029d1e464acd7df
w205 <w205@ip-172-31-29-219.ec2.internal>
2017-02-24 06:10:21 +0000

    2/23 10:10 PKB

---
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -0,0 +1,18 @@
+SELECT 
+a.measure_id, 
+a.measure_name, 
+MAX(a.score), 
+MIN(a.score), 
+MAX(a.score) - MIN(a.score) AS range_of_score 
+FROM death a  
+JOIN hospital b ON (a.id=b.provider_id) 
+WHERE 
+emergency_services = 'Yes' 
+AND hospital_type = 'Acute Care Hospitals' 
+AND a.measure_start = '07/01/2011' 
+GROUP BY 
+a.measure_id, 
+measure_name 
+ORDER BY x DESC
+LIMIT 10;
+
---
+++ exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
@@ -0,0 +1,41 @@
+Drop Table h_grade; 
+
+CREATE TABLE h_grade AS 
+SELECT 
+a.id,  
+a.hospital, 
+CASE WHEN  a.comp = 'Better than the National Rate' THEN 1 ELSE 0 END as better_than_score, 
+CASE WHEN a.comp = 'Worse than the National Rate' THEN 1 ELSE 0 END AS worse_than_score 
+FROM death a
+JOIN hospital b ON (a.id=b.provider_id) 
+WHERE 
+emergency_services = 'Yes' 
+AND hospital_type = 'Acute Care Hospitals';
+
+DROP TABLE h_grade_group;
+
+CREATE TABLE h_grade_group AS 
+SELECT id,
+hospital, 
+sum(better_than_score) - sum(worse_than_score) as final_grade 
+From h_grade 
+GROUP BY 
+id, 
+hospital;
+
+SELECT 
+final_grade, 
+count(id) 
+FROM h_grade_group 
+Group by final_grade 
+ORDER BY final_grade;
+
+SELECT a.final_grade, 
+AVG(patient_score) 
+FROM h_grade_group a 
+JOIN hospital_avg_score b ON a.id=b.id 
+GROUP BY a.final_grade 
+ORDER BY a.final_grade;
+
+
+
