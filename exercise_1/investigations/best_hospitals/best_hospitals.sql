a0aecfea082d320a83f9e7f1d010efe478f5623a
w205 <w205@ip-172-31-29-219.ec2.internal>
2017-02-26 00:12:21 +0000

    2/25 4:12 PKB

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,13 +1,17 @@
 Select hospital_type From hospital Group by hospital_type;
 
 SELECT 
-a.hospital, 
-a.quality_score 
+a.id AS id,
+a.hospital AS hospital,
+a.state AS state, 
+a.quality_score  AS quality_of_care
 FROM hospital_avg_score a 
 JOIN hospital b ON (a.id=b.provider_id) 
 WHERE emergency_services = 'Yes' AND hospital_type = 'Acute Care Hospitals' 
-GROUP BY 
+GROUP BY
+a.id, 
 a.hospital, 
+a.state,
 a.quality_score 
 ORDER BY quality_score DESC 
 LIMIT 10;
